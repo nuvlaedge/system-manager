4315d78dd6e496b02cb25ad2ba6c5ef92feb3487
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-07-29 12:20:58 +0200

    finish local dashboard

--- code/app.py
+++ code/app.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
 
         while True:
             supervisor.build_content()
-            time.sleep(3)
+            time.sleep(2)
 
 
 
--- code/system_manager/Supervise.py
+++ code/system_manager/Supervise.py
@@ -35,7 +35,7 @@ class Supervise(object):
         """ Pretty prints """
 
         with open(self.printer_file, 'w') as p:
-            p.write("{} \n\n{}".format(content, time.ctime()))
+            p.write("{}".format(content))
 
     def build_content(self):
         """ Builds the HTML content for the web server """
@@ -45,35 +45,51 @@ class Supervise(object):
                   '<html>' \
                   '<head>' \
                   '<style>' \
-                  ' body {{background-color: powderblue;}}' \
-                  ' #top {{font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;}}' \
+                  ' body {{background-color: #001f3f;' \
+                  '         color: #7dbbfb;' \
+                  '         margin-top: 0;}}' \
+                  ' #top {{font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;' \
+                  '         margin-top: 0;}}' \
+                  ' #upper {{font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;' \
+                  '         margin: 0;}}' \
                   ' #stats {{font-family: "Courier New", Courier, monospace;' \
                   '          border-spacing: 20px 5px;}}' \
+                  ' #top td:first-child {{text-align: right;}}' \
                   '</style>' \
                   '</head>' \
                   '<body>' \
+                  '<div id="upper">' \
+                  '<center><i style="color:white;">Last updated: {}</i></center> <br>' \
                   '<table id="top" align="center">' \
-                  '<tr><td><b>HOSTNAME:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>CONTAINERS RUNNING:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>NUVLABOX CONTAINERS RUNNING:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>LOCAL IMAGES:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>OPERATING SYSTEM:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>ARCHITECTURE:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>DEVICE SPECS:</b></td> <td>{}</td></tr> <br>' \
-                  '<tr><td><b>ADDRESS:</b></td> <td>{}</td></tr> <br>' \
-                  '</table><br><hr><br>' \
+                  '<tr><td><b>HOSTNAME:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>CONTAINERS RUNNING:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>NUVLABOX CONTAINERS RUNNING:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>LOCAL IMAGES:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>DOCKER DISK USAGE:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>OPERATING SYSTEM:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>ARCHITECTURE:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>DEVICE SPECS:</b></td> <td>{}</td></tr> ' \
+                  '<tr><td><b>ADDRESS:</b></td> <td>{}</td></tr> ' \
+                  '</table><br></div><hr><br>' \
                   '{}' \
-            .format(info['Name'],
+            .format(time.ctime(),
+                    info['Name'],
                     info['ContainersRunning'],
                     len(self.list_internal_containers()),
                     info['Images'],
+                    "%.2f GB" % self._get_docker_disk_usage(),
                     info['OperatingSystem'],
                     info['Architecture'],
-                    "%s CPUs and %s GiB of memory" % (info['NCPU'], info['MemTotal']),
+                    "%s CPUs and %s GiB of memory" % (info['NCPU'], round((info['MemTotal']/1024/1024/1024), 2)),
                     info['Swarm']['NodeAddr'],
                     self._get_stats_table_html())
 
         self.printer(content)
+
+    def _get_docker_disk_usage(self):
+        """ Runs docker system df and gets disk usage """
+
+        return round(float(self.docker_client.df()["LayersSize"] / 1000 / 1000 / 1000), 2)
 
     def _get_docker_info(self):
         """ Gets everything from the Docker client info """
@@ -91,9 +107,14 @@ class Supervise(object):
                 '<th>MEM USAGE/LIMIT</th>' \
                 '<th>MEM %</th>' \
                 '<th>NET I/O</th>' \
-                '<th>BLOCK I/O</th></tr>'
+                '<th>BLOCK I/O</th>' \
+                '<th>STATUS</th>' \
+                '<th>RESTARTED</th></tr>'
+
         for container in self.docker_client.containers.list():
             previous_cpu = previous_system = cpu_percent = mem_percent = mem_usage = mem_limit = net_in = net_out = blk_in = blk_out = 0.0
+            restart_count = 0
+            container_status = "unknown"
             x = 0
             for container_stats in self.docker_client.api.stats(container.id, stream=True, decode=True):
                 cpu_percent = 0.0
@@ -114,22 +135,26 @@ class Supervise(object):
                     if round(mem_limit, 2) == 0.00:
                         mem_percent = 0.00
                     else:
-                        mem_percent = round(float(mem_usage / mem_limit), 2)
+                        mem_percent = round(float(mem_usage / mem_limit) * 100, 2)
                     net_in = sum(container_stats["networks"][iface]["rx_bytes"] for iface in container_stats["networks"]) / 1000 / 1000
                     net_out = sum(container_stats["networks"][iface]["tx_bytes"] for iface in container_stats["networks"]) / 1000 / 1000
                     blk_in = float(container_stats["blkio_stats"]["io_service_bytes_recursive"][0]["value"] / 1000 / 1000)
                     blk_out = float(container_stats["blkio_stats"]["io_service_bytes_recursive"][1]["value"] / 1000 / 1000)
+                    container_status = container.status
+                    restart_count = int(container.attrs["RestartCount"])
                     break
 
             stats += '<tr>' \
-                     '<td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td>' \
+                     '<td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td> <td>{}</td>' \
                      '</tr>'.format(container.id[:12],
-                                    container.name,
-                                    round(cpu_percent, 2),
-                                    "%s MiB / %s GiB" % (round(mem_usage, 2), round(mem_limit / 1024, 2)),
-                                    "%s %%" % mem_percent,
-                                    "%s MB / %s MB" % (round(net_in, 2), round(net_out, 2)),
-                                    "%s MB / %s MB" % (round(blk_in, 2), round(blk_out, 2)))
+                                    container.name[:25],
+                                    "%.2f%%" % round(cpu_percent, 2),
+                                    "%sMiB / %sGiB" % (round(mem_usage, 2), round(mem_limit / 1024, 2)),
+                                    "%.2f%%" % mem_percent,
+                                    "%sMB / %sMB" % (round(net_in, 2), round(net_out, 2)),
+                                    "%sMB / %sMB" % (round(blk_in, 2), round(blk_out, 2)),
+                                    container_status,
+                                    restart_count)
 
         stats += '</table>'
 
