150a81d7caf38c73e524f50824d7f4b6b4f3d051
Cristovao Cordeiro <cris@sixsq.com>
2021-03-03 08:41:43 +0100

    adjust min requirements - remove swarm

--- code/run.py
+++ code/run.py
@@ -41,20 +41,24 @@ on_stop = GracefulShutdown()
 
 
 def run_requirements_check():
+    utils.set_operational_status(utils.status_unknown)
     if not MinReq.SKIP_MINIMUM_REQUIREMENTS:
         # Check if the system complies with the minimum hw and sw requirements for the NuvlaBox
         system_requirements = MinReq.SystemRequirements()
         software_requirements = MinReq.SoftwareRequirements()
 
         if not software_requirements.check_docker_requirements() or not system_requirements.check_all_hw_requirements():
-            log.error("System does not meet the minimum requirements! Stopping")
-            utils.cleanup(utils.list_internal_containers(), utils.docker_id)
-            sys.exit(1)
+            log.error("System does not meet the minimum requirements!")
+            utils.set_operational_status(utils.status_degraded)
+            # utils.cleanup(utils.list_internal_containers(), utils.docker_id)
+            # sys.exit(1)
+        else:
+            utils.set_operational_status(utils.status_operational)
     else:
         log.warning("You've decided to skip the system requirements verification. "
                     "It is not guaranteed that the NuvlaBox will perform as it should. Continuing anyway...")
+        utils.set_operational_status(utils.status_operational)
 
-    utils.set_operational_status("OPERATIONAL")
     log.info("Successfully created status file")
 
     peripherals = '{}/.peripherals'.format(utils.data_volume)
