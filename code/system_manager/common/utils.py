59c10ebe85fdcd5c311600bd26dd49623c1789b3
schaubl <schaubl@users.noreply.github.com>
2023-04-03 19:58:12 +0200

    Remove COMPOSE_PROJECT and keep only COMPOSE_PROJECT_NAME

--- code/system_manager/common/utils.py
+++ code/system_manager/common/utils.py
@@ -14,7 +14,7 @@ operational_status_notes_file = f'{data_volume}/.status_notes'
 base_label = "nuvlaedge.component=True"
 node_label_key = "nuvlaedge"
 
-compose_project_name = os.getenv('COMPOSE_PROJECT', os.getenv('COMPOSE_PROJECT_NAME', 'nuvlaedge'))
+compose_project_name = os.getenv('COMPOSE_PROJECT_NAME', 'nuvlaedge')
 nuvlaedge_shared_net = compose_project_name + '-shared-network'
 nuvlaedge_shared_net_unencrypted = f'{data_volume}/.nuvlabox-shared-net-unencrypted'
 overlay_network_service = 'nuvlaedge-ack'
